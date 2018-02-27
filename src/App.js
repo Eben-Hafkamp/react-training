67709277a581f5a55d41ea2508c0589cbd416bc7
Eben <ebenhafkamp@gmail.com>
2018-02-27 23:25:01 +1300

    char task complete

--- src/App.js
+++ src/App.js
@@ -13,45 +13,32 @@ class App extends Component {
   character_arr;
 
   input_changed_handler = (event) => {
-
-    let character_str = '';
-
-    if (typeof(event) !== 'undefined') {
-      character_str = event.target.value;
-      this.character_arr = event.target.value.split('');
-    } 
-
+    this.character_arr = event.target.value.split('');
     this.setState({
-      input_value: character_str,
+      input_value: event.target.value,
       characters: this.character_arr
     });
-
-    console.log(this.state.input_value.length);
-
   }
 
   delete_clicked_char_handler = (index) => {
-
     this.character_arr.splice(index, 1);
-    
-    this.input_changed_handler();
-
+    this.setState({
+      input_value: this.character_arr.join(''),
+      characters: this.character_arr
+    });
   }
 
   render() {
+    // Can also store JSX components over here in a variable (array) and then spit it out with {} below
     return (
       <div className="UsernameBuilder">
         <p>There are { this.state.input_value.length } characters.</p>
-        <input 
-        type="text" 
-        onChange={ (event) => this.input_changed_handler(event) } 
-        value={ this.state.characters.join('') }/>
+        <input type="text" onChange={ this.input_changed_handler } value={ this.state.characters.join('') }/>
         <ValidationComponent length={ this.state.input_value.length } />
-        { 
-          /* Loop over the list of characters and ouput them as CharComponents in JSX */
+        { /* Loop over the list of characters and ouput them as CharComponents in JSX */
           this.state.characters.map((char, index) => {
-            return <CharComponent 
-            click={ () => this.delete_clicked_char_handler(index) } // JSX component requires method reference
+            return <CharComponent // JSX component requires method reference
+            click={ () => this.delete_clicked_char_handler(index) } 
             key={ index } 
             character={ char } />
           })
